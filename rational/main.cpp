bb01819efef7e518ae0e24d9af3e8f19bb8c0584
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2024-06-03 20:53:51 +0000

    Add main from lab 1

--- rational/main.cpp
+++ rational/main.cpp
@@ -1,34 +1,66 @@
+// Archivo: main.cpp
+
+// Para compilar y correr:
+//
+//     g++ *.cpp -o main && ./main
+
 #include <iostream>
+#include <iomanip>
 #include <stdexcept>
 #include "rational.h"
 
 int main()
 {
-    Rational r(1, 3);
-    Rational s(1, 2);
-    std::cout << "r = " << r << "\n";
-    std::cout << "s = " << s << "\n";
-    std::cout << "r + s + r = " << r + s + r << "\n";
+    Rational a(1, 2);
+    Rational b(6, 4);
+    Rational c(4, 8);
+    Rational d;
+    Rational e(-3);
 
-    Rational t(3, 9);
-    std::cout << "r igual a t: " << (r == t) << "\n";
-    std::cout << "r igual a s: " << (r == s) << "\n";
-    std::cout << "r no es igual a t: " << (r != t) << "\n";
-    std::cout << "r no es igual a s: " << (r != s) << "\n";
-    std::cout << "-r = " << -r << "\n";
-    std::cout << "s - r = " << s - r << "\n";
+    // Para imprimir los booleanos como true o false
+    // en lugar de 1 o 0.
+    std::cout << std::boolalpha;
 
-    std::cout << "r convertido a double: " << static_cast<double>(r) << "\n";
-    std::cout << "s convertido a double: " << static_cast<double>(s) << "\n";
-    std::cout << "t convertido a double: " << static_cast<double>(t) << "\n";
-
-    try {
-        Rational u(4, 0);
-        std::cout << "u = " << u << "\n";
-    } catch (std::invalid_argument e) {
-        std::cout << "Error detected: " << e.what() << "\n";
-    }
-
-    std::cout << "Fin de programa\n";
+    std::cout << "a: " << a << "\n";
+    std::cout << "b: " << b << "\n";
+    std::cout << "c: " << c << "\n";
+    std::cout << "d: " << d << "\n";
+    std::cout << "e: " << e << "\n";
+    std::cout << "a + b: " << a + b << "\n";
+    std::cout << "c + d: " << c + d << "\n";
+    std::cout << "a == c: " << (a == c) << "\n";
+    std::cout << "a == b: " << (a == b) << "\n";
+    std::cout << "a != b: " << (a != b) << "\n";
+    std::cout << "a != c: " << (a != c) << "\n";
+    std::cout << "-a: " << -a << "\n";
+    std::cout << "-e: " << -e << "\n";
+    std::cout << "a - b: " << a - b << "\n";
+    std::cout << "c - e: " << c - e << "\n";
+    std::cout << "static_cast<double>(a): "
+        << static_cast<double>(a) << "\n";
+    std::cout << "static_cast<double>(b): "
+        << static_cast<double>(b) << "\n";
+    // std::cout << "a * b: " << a * b << "\n";
+    // std::cout << "c * e: " << c * e << "\n";
+    // std::cout << "~b: " << ~b << "\n";
+    // std::cout << "~e: " << ~e << "\n";
+    // std::cout << "b * ~b: " << b * ~b << "\n";
+    // std::cout << "e * ~e: " << e * ~e << "\n";
+    // std::cout << "a / b: " << a / b << "\n";
+    // std::cout << "c / e: " << c / e << "\n";
+    // try {
+    //     Rational t = a / d;
+    //     std::cout << "a / d: " << t << "\n";
+    // } catch (const std::invalid_argument& e) {
+    //     std::cout << "a / d: " << e.what() << "\n";
+    // }
+    // std::cout << "a < b: " << (a < b) << "\n";
+    // std::cout << "a < c: " << (a < c) << "\n";
+    // std::cout << "a <= c: " << (a <= c) << "\n";
+    // std::cout << "a <= d: " << (a <= d) << "\n";
+    // std::cout << "b > c: " << (b > c) << "\n";
+    // std::cout << "a > c: " << (a > c) << "\n";
+    // std::cout << "a >= c: " << (a >= c) << "\n";
+    // std::cout << "e >= d: " << (e >= d) << "\n";
     return 0;
 }

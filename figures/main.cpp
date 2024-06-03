289f165ca3324a65fa05e745bda58142fb4d69f4
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2024-06-03 20:54:12 +0000

    Add code to implement Square class

--- figures/main.cpp
+++ figures/main.cpp
@@ -2,9 +2,21 @@
 #include "figure.h"
 #include "rectangle.h"
 #include "circle.h"
+#include "square.h"
 
 int main() {
+    Figure *f;
     Circle c(2.0);
-    std::cout << c << "\n";
+    f = &c;
+    std::cout << c.area() << "\n";
+    std::cout << f->area() << "\n";
+    Rectangle r(3.0, 6.0);
+    std::cout << r.area() << "\n";
+    f = &r;
+    std::cout << f->area() << "\n";
+    Square s(3.0);
+    std::cout << s << "\n";
+    f = &s;
+    std::cout << f->area() << "\n";
     return 0;
 }
